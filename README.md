442db0271549fc4e5aa3d7a0af710781c86c27da
mbui2 <mbui2@hawk.iit.edu>
2022-04-26 14:59:34 -0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-3-final
+# project3-first
---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*function init(){
+  function myEventFunction(){
+    alert('Welcome to my Page!');
+  }
+  var a1 = document.getElementById("header");
+
+  a1.addEventListener('click', myEventFunction);
+  a1.addEventListener('click',function(){
+ 	  console.log('Thanks for clicking!');
+    a1.style.background = 'rgb(165,105,189)';
+    a1.style.color = 'rgb(28,25,15)';
+  });
+}
+window.addEventListener('load', init);*/
